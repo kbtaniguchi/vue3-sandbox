b6ca69ee0106c68dac1bd879408224852de92256
Kiminobu Taniguchi <k_taniguchi@actier.co.jp>
2022-07-16 12:16:30 +0900

    fix format

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
-import {createApp} from 'vue'
-import {createPinia} from 'pinia'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
 import App from '@/App.vue'
 import router from '@/router'
 
 import '@/assets/tailwind.css'
 
-import {worker} from '@/mocks/browser'
+import { worker } from '@/mocks/browser'
 
 if (process.env.NODE_ENV === 'msw') {
-    await worker.start();
+  await worker.start()
 }
 
 const app = createApp(App)
