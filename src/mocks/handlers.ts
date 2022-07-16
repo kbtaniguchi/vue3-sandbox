f2180ab632430b31e3d332c2232fce5d4ba52c08
Kiminobu Taniguchi <k_taniguchi@actier.co.jp>
2022-07-16 12:15:38 +0900

    npm install -D prettier eslint-config-prettier

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,19 +1,20 @@
-import {rest} from 'msw'
+import { rest } from 'msw'
 
 export const handlers = [
-    rest.get('/api/error/:status', (req, res, ctx) => {
-        const {status} = req.params
-        if (typeof status === "string") return res(
-            ctx.status(Number(status)),
-            ctx.json({
-                message: "error"
-            }),
-        )
-        return res(
-            ctx.status(500),
-            ctx.json({
-                message: "error"
-            }),
-        )
-    }),
+  rest.get('/api/error/:status', (req, res, ctx) => {
+    const { status } = req.params
+    if (typeof status === 'string')
+      return res(
+        ctx.status(Number(status)),
+        ctx.json({
+          message: 'error',
+        })
+      )
+    return res(
+      ctx.status(500),
+      ctx.json({
+        message: 'error',
+      })
+    )
+  }),
 ]
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,8 +2,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes: [
-  ]
+  routes: [],
 })
 
 export default router
